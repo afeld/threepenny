8a38b0ab4bcbb2d7c4876d72de4967c78f8d3766
Aidan Feldman <aidan.feldman@gmail.com>
2010-12-05 23:55:39 -0500

    fixed boilerplate views

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,7 @@
 class Country < ActiveRecord::Base
   has_many :loans, :dependent => :nullify
+  
+  def to_s
+    self.name
+  end
 end
--- app/models/loan_sector.rb
+++ app/models/loan_sector.rb
@@ -1,3 +1,7 @@
 class LoanSector < ActiveRecord::Base
   has_many :loans, :dependent => :nullify
+  
+  def to_s
+    self.name
+  end
 end
