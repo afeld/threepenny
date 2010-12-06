788e217c1af924eecb95e93253869692c9dc12bc
Aidan Feldman <aidan.feldman@gmail.com>
2010-12-05 22:41:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threepenny::Application.config.secret_token = '8d93668105e213294a2f744c698980952c468b695c75beb0e2ea84d261b1b053a33822529f35cd405cc324f725a2645fae44545571d4092e6b972b0eaa9c55a9'
