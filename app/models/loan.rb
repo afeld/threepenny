a14f42f034886caa39113d0ad37f0eae74dac154
Aidan Feldman <aidan.feldman@gmail.com>
2010-12-06 00:34:45 -0500

    override latitude and longitude

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,4 +3,12 @@ class Loan < ActiveRecord::Base
   belongs_to :loan_sector
   
   validates :kiva_id, :uniqueness => true
+  
+  def latitude
+    super || self.country.latitude if self.country
+  end
+  
+  def longitude
+    super || self.country.longitude if self.country
+  end
 end
